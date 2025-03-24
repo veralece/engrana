f70448c991a7735ff04b78d9c2fef4c6acc80c09
Javier Penaloza <veralece@gmail.com>
2025-03-23 22:59:03 -0700

    updated save functionality on context

--- api/Infrastructure/EngranaContext.cs
+++ api/Infrastructure/EngranaContext.cs
@@ -7,5 +7,35 @@ public class EngranaContext(DbContextOptions<EngranaContext> options) : DbContex
 {
     public DbSet<Asset> Asset { get; set; }
     public DbSet<ConfigurationItem> ConfigurationItem { get; set; }
+
     // public DbSet<Manufacturer> Manufacturer { get; set; }
+
+    // protected override void OnModelCreating(ModelBuilder modelBuilder)
+    // {
+
+    // }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        foreach (var entry in ChangeTracker.Entries<EntityBase>())
+        {
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    entry.Entity.Created = DateTime.Now.ToUniversalTime();
+                    entry.Entity.LastModified = DateTime.Now.ToUniversalTime();
+                    break;
+                case EntityState.Modified:
+                    entry.Entity.LastModified = DateTime.Now.ToUniversalTime();
+                    break;
+                case EntityState.Deleted:
+                    entry.State = EntityState.Modified;
+                    entry.Entity.IsDeleted = true;
+                    entry.Entity.Deleted = DateTime.Now.ToUniversalTime();
+                    entry.Entity.LastModified = DateTime.Now.ToUniversalTime();
+                    break;
+            }
+        }
+        return await base.SaveChangesAsync(new CancellationToken());
+    }
 }
