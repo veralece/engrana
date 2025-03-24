c395973386313286d5992d528d6cb4988a262166
Javier Penaloza <veralece@gmail.com>
2025-03-23 22:58:13 -0700

    new properties added to base entity

--- api/Domain/EntityBase.cs
+++ api/Domain/EntityBase.cs
@@ -3,4 +3,11 @@ namespace Engrana.Domain;
 public abstract class EntityBase
 {
     public Guid Id { get; set; }
+    public DateTime? Created { get; set; }
+    public Guid? CreatedBy { get; set; }
+    public DateTime? LastModified { get; set; }
+    public Guid? LastModifiedBy { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime? Deleted { get; set; }
+    public Guid? DeletedBy { get; set; }
 }
