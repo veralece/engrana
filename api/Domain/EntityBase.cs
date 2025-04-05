9c14001c551959bb14590d3bee958b3ec06aa489
Javier Penaloza <veralece@gmail.com>
2025-04-05 00:33:54 -0700

    added copy functionality

--- api/Domain/EntityBase.cs
+++ api/Domain/EntityBase.cs
@@ -6,6 +6,7 @@ public abstract class EntityBase
 {
     public Guid Id { get; set; }
     public required string Name { get; set; }
+    public EntityType Type { get; set; }
     public DateTime? Created { get; set; }
     public Guid? CreatedBy { get; set; }
     public DateTime? LastModified { get; set; }
@@ -13,16 +14,6 @@ public abstract class EntityBase
     public bool IsDeleted { get; set; }
     public DateTime? Deleted { get; set; }
     public Guid? DeletedBy { get; set; }
-}
 
-public static class EntityFactory
-{
-    public static EntityBase? TryConvertFromEnum(EntityType entityType, EntityBase? entity)
-    {
-        return entityType switch
-        {
-            EntityType.Asset => entity as Asset,
-            _ => null,
-        };
-    }
+    public T ShallowCopy<T>() => (T)this.MemberwiseClone();
 }
