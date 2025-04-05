4a0ae9cedd39450e0116774f68b385717262ceae
Javier Penaloza <veralece@gmail.com>
2025-04-05 00:36:54 -0700

    added entities

--- api/Infrastructure/EngranaContext.cs
+++ api/Infrastructure/EngranaContext.cs
@@ -1,22 +1,51 @@
 using Engrana.Domain;
+using Engrana.Domain.Configuration;
 using Microsoft.EntityFrameworkCore;
 
 namespace Engrana.Infrastructure;
 
 public class EngranaContext(DbContextOptions<EngranaContext> options) : DbContext(options)
 {
+    #region Domain
+
     public DbSet<Asset> Asset { get; set; }
+    public DbSet<Change> Change { get; set; }
     public DbSet<ConfigurationItem> ConfigurationItem { get; set; }
     public DbSet<ContactInformation> ContactInformation { get; set; }
     public DbSet<Incident> Incident { get; set; }
+    public DbSet<KnowledgeArticle> KnowledgeArticle { get; set; }
     public DbSet<Manufacturer> Manufacturer { get; set; }
     public DbSet<Organization> Organization { get; set; }
     public DbSet<PhysicalAddress> PhysicalAddress { get; set; }
+    public DbSet<ServiceRequest> ServiceRequest { get; set; }
 
-    // protected override void OnModelCreating(ModelBuilder modelBuilder)
-    // {
+    #endregion
+    #region Domain.Configuration
 
-    // }
+
+    public DbSet<Workflow> Workflow { get; set; }
+    public DbSet<WorkflowStep> WorkflowStep { get; set; }
+    public DbSet<Trigger> Trigger { get; set; }
+    public DbSet<CompareStatement> CompareStatement { get; set; }
+    public DbSet<StringPropertyState> PropertyStateString { get; set; }
+    public DbSet<StringProperty> StringProperty { get; set; }
+    public DbSet<StringSet> StringSet { get; set; }
+    public DbSet<StringEntry> StringEntry { get; set; }
+    public DbSet<DatePropertyState> PropertyStateDate { get; set; }
+    public DbSet<DateProperty> DateProperty { get; set; }
+    public DbSet<DateSet> DateSet { get; set; }
+    public DbSet<DateEntry> DateEntry { get; set; }
+    public DbSet<NumberPropertyState> PropertyStateNumber { get; set; }
+    public DbSet<NumberProperty> NumberProperty { get; set; }
+    public DbSet<NumberSet> NumberSet { get; set; }
+    public DbSet<NumberEntry> NumberEntry { get; set; }
+    public DbSet<BooleanPropertyState> PropertyStateBoolean { get; set; }
+    public DbSet<BooleanProperty> BooleanProperty { get; set; }
+    public DbSet<BooleanEntry> BooleanEntry { get; set; }
+
+    #endregion
+
+    // protected override void OnModelCreating(ModelBuilder modelBuilder) { }
 
     public async Task<int> SaveChangesAsync()
     {
